276bdd0503a0b79a8f2a19f4f7f78689be71e535
PolarJHONG <109599109+PolarJHONG@users.noreply.github.com>
2022-07-24 22:08:46 +0800

    Add files via upload

---
+++ scripts/playlist.js
@@ -0,0 +1,22 @@
+trackList = [
+	{
+		name:"Ed Sheeran1",
+		artist:"Ed Sheeran2",
+		src:"Ed Sheeran.mp3"
+	},
+	{
+		name:"Ed Sheeran2",
+		artist:"Ed Sheeran2",
+		src:"Ed Sheeran1.mp3"
+	},	
+	
+		name:"Ed Sheeran3",
+		artist:"Ed Sheeran2",
+		src:"Ed Sheeran2.mp3"
+	},	
+	{
+		name:"Ed Sheeran4",
+		artist:"Ed Sheeran2",
+		src:"Ed Sheeran3.mp3"
+	},
+];
