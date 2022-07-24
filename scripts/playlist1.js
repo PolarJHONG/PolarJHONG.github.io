fecdf6a5d7f2d0b2a1d0f8f2b69fddb144b1a36b
PolarJHONG <109599109+PolarJHONG@users.noreply.github.com>
2022-07-24 23:12:58 +0800

    Add files via upload

---
+++ scripts/playlist1.js
@@ -0,0 +1,58 @@
+trackList = [
+	{
+		name:"Selena Gomez - Back To You",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - Back To You.mp3"
+	},
+	{
+		name:"Selena Gomez - Come Get It",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - Come Get It.mp3"
+	},
+	{
+		name:"Selena Gomez - Dance Again (Performance Video)",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - Dance Again (Performance Video).mp3"
+	},
+	{
+		name:"Selena Gomez - Fetish ft. Gucci Mane (Official Music Video)",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - Fetish ft. Gucci Mane (Official Music Video).mp3"
+	},
+	{
+		name:"Selena Gomez - Lose You To Love Me (Official Music Video)",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - Lose You To Love Me (Official Music Video).mp3"
+	},
+	{
+		name:"Selena Gomez - Rare (Official Music Video)",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - Rare (Official Music Video).mp3"
+	},
+	{
+		name:"Selena Gomez - Same Old Love",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - Same Old Love.mp3"
+	},
+	{
+		name:"Selena Gomez - Slow Down (Official)",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - Slow Down (Official).mp3"
+	},
+	{
+		name:"Selena Gomez - The Heart Wants What It Wants (Official Video)",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - The Heart Wants What It Wants (Official Video).mp3"
+	},
+	{
+		name:"Selena Gomez - Who Says",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez - Who Says.mp3"
+	},
+	{
+		name:"Selena Gomez, Marshmello - Wolves",
+		artist:"Selena Gomez",
+		src:"Selena Gomez/Selena Gomez, Marshmello - Wolves.mp3"
+	},
+	
+];
