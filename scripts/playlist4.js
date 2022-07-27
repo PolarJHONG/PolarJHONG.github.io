327dc54a24ab9c873e32c4a37f87ee2b190e9d14
PolarJHONG <109599109+PolarJHONG@users.noreply.github.com>
2022-07-27 21:00:32 +0800

    Add files via upload

---
+++ scripts/playlist4.js
@@ -0,0 +1,77 @@
+trackList = [
+	{
+		name:" CHEN(첸)XPunch(펀치) - Everytime",
+		artist:"",
+		src:"korea/[MV] CHEN(첸)XPunch(펀치) - Everytime l 태양의 후예 OST Part.2.mp3"
+	},
+	{
+		name:" IU(아이유) _ BBIBBI",
+		artist:"",
+		src:"korea/[MV] IU(아이유) _ BBIBBI(삐삐).mp3"
+	},
+	{
+		name:"IU(아이유) _ Blueming",
+		artist:"",
+		src:"korea/[MV] IU(아이유) _ Blueming(블루밍).mp3"
+	},
+	{
+		name:"IU(아이유) _ Celebrity",
+		artist:"",
+		src:"korea/[MV] IU(아이유) _ Celebrity.mp3"
+	},
+	{
+		name:"IU(아이유) _ eight",
+		artist:"",
+		src:"korea/[MV] IU(아이유) _ eight(에잇) (Prod.Feat. SUGA of BTS)"
+	},
+	{
+		name:"IU(아이유)_LILAC",
+		artist:"",
+		src:"korea/[MV] IU(아이유)_LILAC(라일락)"
+	},
+	{
+		name:"Punch - Another Day",
+		artist:"",
+		src:"korea/[MV] 먼데이 키즈(Monday Kiz), 펀치(Punch) - Another Day (호텔 델루나 OST) Hotel Del Luna OST Part 1.mp3"
+	},
+	{
+		name:"Punch - Go away go away",
+		artist:"",
+		src:"korea/[MV] 찬열 (CHANYEOL), 펀치 (Punch) - Go away go away [낭만닥터 김사부 2 (Dr. Romantic 2) OST Part.3]"
+	},
+	{
+		name:"Punch - Say Yes",
+		artist:"",
+		src:"korea/[달의 연인 - 보보경심 려 OST Part 2] 로꼬, 펀치 (Loco, Punch) - Say Yes MV"
+	},
+	{
+		name:"PUNCH - Stay With Me",
+		artist:"",
+		src:"korea/[도깨비 OST Part 1] 찬열, 펀치 (CHANYEOL, PUNCH) - Stay With Me MV"
+	},
+	{
+		name:"Punch - Done For Me",
+		artist:"",
+		src:"korea/Punch - Done For Me ( tvN 德魯納酒店 Hotel Del Luna OST Part.12 ) MV.mp3"
+	},
+	{
+		name:"IU Good Day",
+		artist:"",
+		src:"korea/IU Good Day Lyrics (아이유 좋은 날 가사).mp3"
+	},
+	{
+		name:"Punch - Done For Me",
+		artist:"",
+		src:"korea/Punch - Done For Me ( tvN 德魯納酒店 Hotel Del Luna OST Part.12 ) MV.mp3"
+	},
+	{
+		name:"IU-YOU&I",
+		artist:"",
+		src:"korea/IU(아이유) _ YOUI(너랑 나) (Performance ver.).mp3"
+	},
+	{
+		name:"Punch -Bye Bye",
+		artist:"",
+		src:"korea/Punch -Bye Bye.mp3"
+	},
+];
