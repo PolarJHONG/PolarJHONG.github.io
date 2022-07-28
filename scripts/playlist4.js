503dec336ae395be731cda66d060bef6616914d8
PolarJHONG <109599109+PolarJHONG@users.noreply.github.com>
2022-07-28 20:53:52 +0800

    Add files via upload

---
+++ scripts/playlist4.js
@@ -0,0 +1,73 @@
+trackList = [
+	{
+		name:"IU -eight",
+		artist:"",
+		src:"korea/IU -eight.mp3"
+	},
+	{
+		name:"IU-Good Day.mp3",
+		artist:"",
+		src:"korea/IU-Good Day.mp3"
+	},
+	{
+		name:"IU-BBIBBI",
+		artist:"",
+		src:"korea/IU-BBIBBI.mp3"
+	},
+	{
+		name:"IU-Blueming",
+		artist:"",
+		src:"korea/IU-Blueming.mp3"
+	},
+	{
+		name:"IU-Celebrity.mp3",
+		artist:"",
+		src:"korea/IU-Celebrity.mp3"
+	},
+	{
+		name:"IU-LILAC",
+		artist:"",
+		src:"korea/IU-LILAC.mp3"
+	},
+	{
+		name:"IU-YOU&I",
+		artist:"",
+		src:"korea/IU-YOU&I.mp3"
+	},
+	{
+		name:"Punch - Done For Me",
+		artist:"",
+		src:"korea/Punch - Done For Me.mp3"
+	},
+	{
+		name:"Punch - Go away go away",
+		artist:"",
+		src:"korea/Punch - Go away go away.mp3"
+	},
+	{
+		name:"PUNCH - Stay With Me",
+		artist:"",
+		src:"korea/PUNCH - Stay With Me.mp3"
+	},
+	{
+		name:"Punch- Another Day",
+		artist:"",
+		src:"korea/Punch- Another Day.mp3"
+	},
+	{
+		name:"Punch -Bye Bye",
+		artist:"",
+		src:"korea/Punch -Bye Bye.mp3"
+	},
+	{
+		name:"Punch- Say Yes",
+		artist:"",
+		src:"korea/Punch- Say Yes.mp3.mp3"
+	},
+	{
+		name:"Punch-Everytime",
+		artist:"",
+		src:"Punch-Everytime.mp3"
+	},
+
+];
